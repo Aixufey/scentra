7f5cc798db531de59afe85cd166d97da5d44c264
Aixufey <jack_11728@msn.com>
2024-10-21 19:58:06 +0200

    init proj

---
+++ src/main/java/com/rinseo/scentra/ScentraApplication.java
@@ -0,0 +1,13 @@
+package com.rinseo.scentra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinseo/scentra/ScentraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinseo.scentra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
