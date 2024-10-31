107a06d6e5783825d639b75cba2dfea8d4f3ebb2
Aixufey <jack_11728@msn.com>
2024-10-31 09:55:26 +0100

    🚥Testing Data Layer REPOSITORY CUSTOM METHODS🚥

--- src/main/java/com/rinseo/scentra/repository/FragranceRepository.java
+++ src/main/java/com/rinseo/scentra/repository/FragranceRepository.java
@@ -2,9 +2,19 @@ package com.rinseo.scentra.repository;
 
 import com.rinseo.scentra.model.Fragrance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FragranceRepository extends JpaRepository<Fragrance, Long> {
     List<Fragrance> findFragrancesByNameContainsIgnoreCase(String name);
+
+    // Default JPQL
+    @Query("SELECT f FROM Fragrance f WHERE f.name LIKE 'A%'")
+    List<Fragrance> findFragrancesStartsWithA();
+
+    // Native SQL
+    @Query(value = "SELECT * FROM Fragrance WHERE name = :name", nativeQuery = true)
+    Fragrance findByName(@Param("name") String name);
 }
---
+++ src/test/java/com/rinseo/scentra/repository/FragranceRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.rinseo.scentra.repository;
+
+import com.rinseo.scentra.model.Fragrance;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+@TestPropertySource(properties = "spring.sql.init.mode=never")
+class FragranceRepositoryTest {
+    @Autowired
+    TestEntityManager entityManager;
+    @Autowired
+    FragranceRepository repo;
+    @Test
+    void testFindFragrance_WhenNameGiven_returnsFragranceEntity() {
+        // Arrange
+        Fragrance alexandriaII = new Fragrance();
+        Fragrance alexandriaIII = new Fragrance();
+
+
+        alexandriaII.setName("Alexandria II");
+        alexandriaII.setYear(2012);
+
+        alexandriaIII.setName("Alexandria III");
+        alexandriaIII.setYear(2019);
+
+        entityManager.persistAndFlush(alexandriaII);
+        entityManager.persistAndFlush(alexandriaIII);
+
+        // Act
+        List<Fragrance> fragrances = repo.findFragrancesByNameContainsIgnoreCase("Alex");
+
+        List<Fragrance> fragrancesStartsWithA = repo.findFragrancesStartsWithA();
+
+        Fragrance fragrance = repo.findByName("Alexandria III");
+
+        // Assert
+        Assertions.assertFalse(fragrances.isEmpty());
+
+        Assertions.assertEquals("Alexandria II", fragrances.stream().findFirst().get().getName());
+
+        Assertions.assertTrue(fragrancesStartsWithA.stream().allMatch(frag -> frag.getName().startsWith("A")));
+
+        Assertions.assertEquals("Alexandria III", fragrance.getName());
+    }
+}
