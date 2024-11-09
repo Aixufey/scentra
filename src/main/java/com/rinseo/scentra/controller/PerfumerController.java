aa3f84ec4a58925a10168d4f858b490d2907a2b8
Aixufey <jack_11728@msn.com>
2024-11-09 17:19:56 +0100

    Update methods in PerfumerService to allow change on inverse side: Perfumer

--- src/main/java/com/rinseo/scentra/controller/PerfumerController.java
+++ src/main/java/com/rinseo/scentra/controller/PerfumerController.java
@@ -1,5 +1,6 @@
 package com.rinseo.scentra.controller;
 
+import com.rinseo.scentra.model.Fragrance;
 import com.rinseo.scentra.model.Perfumer;
 import com.rinseo.scentra.model.dto.PerfumerDTO;
 import com.rinseo.scentra.service.PerfumerServiceImpl;
@@ -21,19 +22,25 @@ public class PerfumerController {
     private PerfumerServiceImpl service;
 
     @GetMapping("/perfumers")
-    public List<Perfumer> getAllPerfumers() {
+    public ResponseEntity<List<Perfumer>> getAllPerfumers() {
         log.info("Fetching all perfumers");
-        return service.getAll();
+        return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("/perfumers/{id}")
-    public Perfumer getPerfumerById(@PathVariable long id) {
-        return service.getById(id);
+    public ResponseEntity<Perfumer> getPerfumerById(@PathVariable long id) {
+        return ResponseEntity.ok(service.getById(id));
     }
 
-    @GetMapping("/perfumers/name/{name}")
-    public List<Perfumer> getPerfumersByName(@PathVariable String name) {
-        return service.getByName(name);
+    @GetMapping("/perfumers/query")
+    public ResponseEntity<List<Perfumer>> getPerfumersByName(@RequestParam(required = false) String name) {
+        if (name != null) {
+            String sanitizedName = name.replaceAll("[^a-zA-Z0-9 ]", "");
+            log.info("Sanitized name: {}", sanitizedName);
+            return ResponseEntity.ok(service.getByName(sanitizedName));
+        } else {
+            return ResponseEntity.ok(service.getAll());
+        }
     }
 
     @PutMapping("/perfumers/{id}")
@@ -61,7 +68,34 @@ public class PerfumerController {
     }
 
     @DeleteMapping("/perfumers/{id}")
-    public void delete(@PathVariable long id) {
+    public ResponseEntity<Void> delete(@PathVariable long id) {
         service.deleteById(id);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+    @GetMapping("/perfumers/{id}/fragrances")
+    public ResponseEntity<List<Fragrance>> getFragranceRelation(@PathVariable long id) {
+        List<Fragrance> fragrancesRelation = service.getFragrancesRelation(id);
+        return ResponseEntity.ok(fragrancesRelation);
+
+    }
+
+    @PutMapping("/perfumers/{id}/fragrances/{fragranceId}")
+    public ResponseEntity<List<Fragrance>> updateFragranceRelation(@PathVariable long id, @PathVariable long fragranceId) {
+        List<Fragrance> fragrancesRelation = service.updateFragranceRelation(id, fragranceId);
+
+        return ResponseEntity.ok(fragrancesRelation);
+    }
+
+    @DeleteMapping("/perfumers/{id}/fragrances/{fragranceId}")
+    public ResponseEntity<Void> deleteFragranceRelation(@PathVariable long id, @PathVariable long fragranceId) {
+        service.deleteFragranceRelation(id, fragranceId);
+
+        return ResponseEntity
+                .noContent()
+                .build();
     }
 }
--- src/main/java/com/rinseo/scentra/service/PerfumerService.java
+++ src/main/java/com/rinseo/scentra/service/PerfumerService.java
@@ -1,5 +1,6 @@
 package com.rinseo.scentra.service;
 
+import com.rinseo.scentra.model.Fragrance;
 import com.rinseo.scentra.model.Perfumer;
 import com.rinseo.scentra.model.dto.PerfumerDTO;
 
@@ -17,4 +18,8 @@ public interface PerfumerService {
     PerfumerDTO update(long id, PerfumerDTO perfumer);
 
     void deleteById(long id);
+
+    List<Fragrance> getFragrancesRelation(long id);
+    List<Fragrance> updateFragranceRelation(long id, long fragranceId);
+    void deleteFragranceRelation(long id, long fragranceId);
 }
