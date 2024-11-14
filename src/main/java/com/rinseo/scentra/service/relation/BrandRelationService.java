6e2a055fefac5d03ec88f1654d8b2e864f6c0042
Aixufey <jack_11728@msn.com>
2024-11-14 21:32:21 +0100

    Delete unused fragrance services

--- src/main/java/com/rinseo/scentra/service/relation/BrandRelationService.java
+++
@@ -1,9 +0,0 @@
-package com.rinseo.scentra.service.relation;
-
-import com.rinseo.scentra.model.Fragrance;
-
-public interface BrandRelationService {
-    Fragrance updateBrandRelation(long fragranceId, long brandId);
-
-    void deleteBrandRelation(long fragranceId, long brandId);
-}
--- src/main/java/com/rinseo/scentra/service/relation/CountryRelationService.java
+++
@@ -1,9 +0,0 @@
-package com.rinseo.scentra.service.relation;
-
-import com.rinseo.scentra.model.Fragrance;
-
-public interface CountryRelationService {
-    Fragrance updateCountryRelation(long fragranceId, long countryId);
-
-    void deleteCountryRelation(long fragranceId, long countryId);
-}
